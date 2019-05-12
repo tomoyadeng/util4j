d005f71aa5c8a4ef77e4c78e56f7909b6688a55d
tomoyadeng <tomoyadeng@gmail.com>
2019-05-12 19:55:35 +0800

    update FunctionRuntimeException

--- util4j-core/src/main/java/com/tomoyadeng/util4j/function/exception/FunctionRuntimeException.java
+++ util4j-core/src/main/java/com/tomoyadeng/util4j/function/exception/FunctionRuntimeException.java
@@ -1,7 +1,25 @@
 package com.tomoyadeng.util4j.function.exception;
 
+/**
+ * FunctionRuntimeException
+ */
 public class FunctionRuntimeException extends RuntimeException {
+  public FunctionRuntimeException() {}
+
+  public FunctionRuntimeException(String message) {
+    super(message);
+  }
+
+  public FunctionRuntimeException(String message, Throwable cause) {
+    super(message, cause);
+  }
+
   public FunctionRuntimeException(Throwable cause) {
     super(cause);
+  }
+
+  public FunctionRuntimeException(
+      String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+    super(message, cause, enableSuppression, writableStackTrace);
   }
 }
