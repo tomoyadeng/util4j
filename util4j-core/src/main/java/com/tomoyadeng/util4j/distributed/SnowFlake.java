e73f5ed039389324eb7b2130850305cea99dc2bb
tomoyadeng <tomoyadeng@gmail.com>
2019-05-12 19:58:52 +0800

    update SnowFlake

--- util4j-core/src/main/java/com/tomoyadeng/util4j/distributed/SnowFlake.java
+++ util4j-core/src/main/java/com/tomoyadeng/util4j/distributed/SnowFlake.java
@@ -1,48 +1,37 @@
 package com.tomoyadeng.util4j.distributed;
 
+/**
+ * Twitter SnowFlake, Java Implementation
+ */
 public class SnowFlake {
   // ==============================Fields===========================================
-  /** 开始时间截 (2015-01-01) */
   private static final long EPOCH = 1420041600000L;
 
-  /** 机器id所占的位数 */
   private static final long WORKER_ID_BITS = 5L;
 
-  /** 数据标识id所占的位数 */
   private static final long DATA_CENTER_ID_BITS = 5L;
 
-  /** 支持的最大机器id，结果是31 (这个移位算法可以很快的计算出几位二进制数所能表示的最大十进制数) */
   private static final long MAX_WORKER_ID = ~(-1L << WORKER_ID_BITS);
 
-  /** 支持的最大数据标识id，结果是31 */
   private static final long MAX_DATA_CENTER_ID = ~(-1L << DATA_CENTER_ID_BITS);
 
-  /** 序列在id中占的位数 */
   private static final long SEQUENCE_BITS = 12L;
 
-  /** 机器ID向左移12位 */
   private static final long WORKER_ID_SHIFT = SEQUENCE_BITS;
 
-  /** 数据标识id向左移17位(12+5) */
   private static final long DATA_CENTER_ID_SHIFT = SEQUENCE_BITS + WORKER_ID_BITS;
 
-  /** 时间截向左移22位(5+5+12) */
   private static final long TIMESTAMP_LEFT_SHIFT =
       SEQUENCE_BITS + WORKER_ID_BITS + DATA_CENTER_ID_BITS;
 
-  /** 生成序列的掩码，这里为4095 (0b111111111111=0xfff=4095) */
   private static final long SEQUENCE_MASK = ~(-1L << SEQUENCE_BITS);
 
-  /** 工作机器ID(0~31) */
   private long workerId;
 
-  /** 数据中心ID(0~31) */
   private long dataCenterId;
 
-  /** 毫秒内序列(0~4095) */
   private long sequence = 0L;
 
-  /** 上次生成ID的时间截 */
   private long lastTimestamp = -1L;
 
   // ==============================Constructors=====================================
@@ -63,7 +52,6 @@ public class SnowFlake {
   public synchronized long nextId() {
     long timestamp = timeGen();
 
-    // 如果当前时间小于上一次ID生成的时间戳，说明系统时钟回退过这个时候应当抛出异常
     if (timestamp < lastTimestamp) {
       throw new RuntimeException(
           String.format(
@@ -71,27 +59,21 @@ public class SnowFlake {
               lastTimestamp - timestamp));
     }
 
-    // 如果是同一时间生成的，则进行毫秒内序列
     if (lastTimestamp == timestamp) {
       sequence = (sequence + 1) & SEQUENCE_MASK;
-      // 毫秒内序列溢出
       if (sequence == 0) {
-        // 阻塞到下一个毫秒,获得新的时间戳
         timestamp = tilNextMillis(lastTimestamp);
       }
     }
-    // 时间戳改变，毫秒内序列重置
     else {
       sequence = 0L;
     }
 
-    // 上次生成ID的时间截
     lastTimestamp = timestamp;
 
-    // 移位并通过或运算拼到一起组成64位的ID
-    return ((timestamp - EPOCH) << TIMESTAMP_LEFT_SHIFT) //
-        | (dataCenterId << DATA_CENTER_ID_SHIFT) //
-        | (workerId << WORKER_ID_SHIFT) //
+    return ((timestamp - EPOCH) << TIMESTAMP_LEFT_SHIFT)
+        | (dataCenterId << DATA_CENTER_ID_SHIFT)
+        | (workerId << WORKER_ID_SHIFT)
         | sequence;
   }
 
